575dc7a726f57413479b7c687cd862a28d272fb3
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-09-22 07:16:12 +0200

    problem Solved

---
+++ Codeforces Submission/September 2021/A. Theatre Square/Problem Solved.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+main(){
+long long int x,y,z,a,b;
+scanf("%lld%lld%lld",&x,&y,&z);
+if(x%z==0){
+    a=(x/z);
+}
+else if(x%z!=0){
+    a=(x/z)+1;
+}
+if(y%z==0){
+    b=(y/z);
+}
+else if(y%z!=0){
+     b=(y/z)+1;
+}
+ 
+ 
+ 
+ 
+printf("%lld\n",a*b);
+ 
+ 
+    return 0;
+}
