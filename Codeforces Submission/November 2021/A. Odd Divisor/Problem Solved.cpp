90683412203aaee891a6b28097aefe3b65b84fa1
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-08 03:39:11 +0200

    Problem Solved

---
+++ Codeforces Submission/November 2021/A. Odd Divisor/Problem Solved.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        long long n;
+        cin>>n;
+       while (n%2==0) n/=2;
+       n!=1? cout<<"YES"<<"\n": cout<<"NO"<<"\n";
+       
+       
+       
+        
+    }
+    
+
+
+    return 0;
+}
