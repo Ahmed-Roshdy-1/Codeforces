328dbeaedfd5e925c036ddcbb4e2c1e86bb59a21
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-15 21:14:01 +0200

    Problem Solved

---
+++ Codeforces Submission/December 2021/A. Most Unstable Array/problem Solved.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <bits\stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while (t--)
+  {
+      int n,m;
+      cin>>n>>m;
+    
+    cout<<min(2,n-1)*m<<endl;
+    
+    
+  }
+  
+    
+    return 0;
+}
