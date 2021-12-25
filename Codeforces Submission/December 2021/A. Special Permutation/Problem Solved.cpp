fb2555b9f544ccf573b042e6781f8e256c4a1557
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-25 17:45:17 +0200

    Problem Solved

---
+++ Codeforces Submission/December 2021/A. Special Permutation/Problem Solved.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits\stdc++.h>
+using namespace std;
+int main(){
+  int t;
+  cin>>t;
+  while (t--)
+  {
+    int n;
+    cin>>n;
+    cout<<n<<" ";
+    for(int i=1;i<n;i++){
+        cout<<i<<" ";
+    }
+    
+    
+    
+  }
+  
+    
+    return 0;
+}
