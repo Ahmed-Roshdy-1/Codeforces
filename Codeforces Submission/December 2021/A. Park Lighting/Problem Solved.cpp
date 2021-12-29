23aa9c55b5c03a708c304b227e6ea3ca1a18b81d
AhmedRoshdy <ahmedroshdy01029@gmail.com>
2021-12-29 20:27:00 +0200

    Problem Solved

---
+++ Codeforces Submission/December 2021/A. Park Lighting/Problem Solved.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n,m;
+        cin>>n>>m;
+         cout<<(n*m+1)/2<<"\n";
+    }
+    
+   
+    return 0;
+}
